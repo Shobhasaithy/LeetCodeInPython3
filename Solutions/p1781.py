e923ba94a25fa73cbacce322ed37802911b217e5
Shobhasaithy <146206260+Shobhasaithy@users.noreply.github.com>
2025-08-06 16:22:50 +0530

    Update p1781.py

--- Solutions/p1781.py
+++ Solutions/p1781.py
@@ -2,17 +2,23 @@ class Solution:
     def beautySum(self, s: str) -> int:
         beauty = 0 
         n = len(s)
+
+        # Iterate over all possible substrings of the string
         for i in range(n): 
-            d = dict()
-            for j in range(i,n):
+            d = dict()  # Dictionary to store character frequencies in current substring
+
+            for j in range(i, n):
+                # Update frequency of the current character
                 if s[j] in d:
                     d[s[j]] += 1 
                 else:
                     d[s[j]] = 1 
 
+                # Compute the beauty of the current substring
+                # Beauty = max frequency - min frequency (excluding 0s)
                 a = max(d.values())
                 b = min(d.values())
 
-                beauty += (a-b)
+                beauty += (a - b)  # Add beauty of this substring to total
 
-        return beauty         
+        return beauty
